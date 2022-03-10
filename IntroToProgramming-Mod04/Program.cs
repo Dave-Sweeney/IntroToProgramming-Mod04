35d0745ce3ad04d74a00c8e490c96f6e99770754
David J. Sweeney <david.j.sweeney@outlook.com>
2022-03-10 12:33:32 -0500

    Updated and refactored code for easier reading.

--- IntroToProgramming-Mod04/Program.cs
+++ IntroToProgramming-Mod04/Program.cs
@@ -13,18 +13,16 @@ namespace AlgorithmDataStructures_CS
         {
             // EX 2 - Task 1.2:  Create an array of floating point values to represent a series of grades
             // Create an array here called grdsArray
-            // EX 2 - Task 1.3:  Add grades to the array
-            float[] grdsArray = new float[] { 90, 85, 93, 70, 100 };
+            float[] grdsArray = new float[5];
 
+            // EX 2 - Task 1.3:  Add grades to the array
+            // Call the addGrades method, passing it the grdsArray
+            addGrades(grdsArray);
+            
             // EX 2 - Task 1.4:  Read grades from the array
             displayGrades(grdsArray);
             
             
-            // Call the addGrades method, passing it the grdsArray
-
-
-
-
             // After adding grades to the array, call the displayGrades method
             // to print out the grades to the console window
             // Use the foreach construct to iterate over the array
@@ -36,58 +34,37 @@ namespace AlgorithmDataStructures_CS
             Stack myStack = new Stack();
 
             // EX 2 - Task 2.3:  Push values to the stack
+            // Call the pushStack() method passing in the grades array for values
             pushStack(grdsArray, myStack);
-            
+
 
             // EX 2 - Task 2.4:  Pop values from the stack
-            myStack.Pop();
-            myStack.Pop();
-
-
-
-            // Call the pushStack() method passing in the grades array for values
-            
-
-
             // Call the popStack() method twice to remove the top two items from stack
             // The popStack method will display each popped item to the console window
-
-
-
+            popStack(myStack);
+            
+            
             // EX 2 - Task 3.2:  Create a new .Net SortedList object called myCourses
             // Create a new SortedList object called myCourses
-            SortedList<string, string> myCourses = new SortedList<string, string>();
+            SortedList myCourses = new SortedList();
 
+            
             // EX 2 - Task 3.3:  Add the following values to the myCourses list
             // CS101 Introduction to Programming
             // CS102 Data Structures and Algorithm Analysis
             // CS201 Introduction to Databases
             // CS301 Introduction to Object-Oriented Programming
-            myCourses.Add("CS101", "Introduction to Computer Science");
-            myCourses.Add("CS102", "Data Structures and Algorithm Analysis");
-            myCourses.Add("CS201", "Introduction to Databases");
-            myCourses.Add("CS301", "Introduction to Object-Oriented Programming");
+            // Call the populateList() method
+            populateList(myCourses);
+
 
             // EX 2 - Task 3.4:  Read values from the list
-            foreach(KeyValuePair<string, string> listItem in myCourses)
-            {
-                Console.WriteLine($"{ listItem.Key } { listItem.Value }");
-            }
-
-            // EX 2 - Task 3.5:  Remove values from the list
-            myCourses.Remove("CS101");
-            
-            // Call the populateList() method
-
-
-
-
             // display a course in the list by passing a key
+            displayList(myCourses, "CS101");
 
-
-
-
+            // EX 2 - Task 5:  Remove values from a list
             // Remove an item from the myCourses list using the key
+            removeListItem(myCourses, "CS301");
 
             Console.ReadLine();
 
@@ -105,9 +82,14 @@ namespace AlgorithmDataStructures_CS
         {
             return isRequired && isAvailable && isElective;
         }
+
         static void addGrades(float[] grdArray)
         {
-
+            grdArray[0] = 90f;
+            grdArray[1] = 85.1f;
+            grdArray[2] = 93.3f;
+            grdArray[3] = 71.1f;
+            grdArray[4] = 100f;
         }
 
         static void displayGrades(float[] grdArray)
@@ -130,23 +112,27 @@ namespace AlgorithmDataStructures_CS
 
         static void popStack(Stack stack)
         {
-            Console.WriteLine("Item removed from the stack: ");
+            Console.WriteLine($"The item { stack.Pop() } was removed from the stack.");
 
         }
 
         static void populateList(SortedList list)
         {
-
+            list.Add("CS101", "Introduction to Computer Science");
+            list.Add("CS102", "Data Structures and Algorithm Analysis");
+            list.Add("CS201", "Introduction to Databases");
+            list.Add("CS301", "Introduction to Object-Oriented Programming");
         }
 
         static void displayList(SortedList list, string key)
         {
-
+            Console.WriteLine($"{ list[key] }");
         }
 
         static void removeListItem(SortedList list, string key)
         {
-
+            Console.WriteLine($"{ list[key] } has been removed.");
+            list.Remove(key);
         }
     }
 }
